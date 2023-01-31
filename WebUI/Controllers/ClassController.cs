130143ee7ee8ed4694ff42d464f185428420f0f4
ayyıldız@software <sihlarsefa7@gmail.com>
2023-01-31 16:56:22 +0300

    Question and Class Controller created

---
+++ WebUI/Controllers/ClassController.cs
@@ -0,0 +1,74 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFreamwork;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class ClassController : Controller
+    {
+        ClassManager _classManager = new ClassManager(new EfCoreClassRepository());
+
+        public IActionResult Index()
+        {
+            return View(new ClassListModel()
+            {
+                //where(x=> x.solution).ToList() Expression oldugu icin filtreleme yapabiliriz
+                Classes = _classManager.GetAll().ToList()
+            });
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(ClassModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var values = new Class
+                {
+                    Name = model.Name,
+                };
+
+                _classManager.Create(values);
+
+                return RedirectToAction("Index", "Class");
+                    
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var values = _classManager.GetById(id);
+            if (values!=null)
+            {
+                _classManager.Delete(values);
+                return RedirectToAction("Index", "Class");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Detail(ClassModel model)
+        {
+            Class values = _classManager.GetById(model.Id);
+
+            if (values == null)
+            {
+                return NotFound();
+            }
+
+            return View(values);
+        }
+
+    }
+}
---
+++ WebUI/Controllers/QuestionController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFreamwork;
+using Microsoft.AspNetCore.Mvc;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class QuestionController : Controller
+    {
+        QuestionManager _questionManager = new QuestionManager(new EfCoreQuestionRepository());
+
+        public IActionResult Index()
+        {
+
+            return View(new QuestionListModel()
+            {
+                //where(x=> x.solution).ToList() Expression oldugu icin filtreleme yapabiliriz
+                Questions = _questionManager.GetAll().ToList()
+            });
+        }
+
+        
+    }
+}
