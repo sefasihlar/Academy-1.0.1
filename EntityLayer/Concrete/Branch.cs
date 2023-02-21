c4e3c20a0ed4b552095ad38a6cac2caba7103a16
mustafacann43@gmail.com <mustafacann43@gmail.com>
2023-02-21 12:33:17 +0300

    created Entity branch table

---
+++ EntityLayer/Concrete/Branch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+	public  class Branch
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int ClassId { get; set; }
+		public Class Class { get; set; }
+
+		public DateTime CreatedDate { get; set; } = DateTime.Now;
+		public DateTime UpdatedDate { get; set; } = DateTime.Now;
+
+		public Boolean Condition { get; set; }
+
+
+	}
+}
